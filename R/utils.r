89fd50f324e9f9454a6c81481b36f8847d4a114d
Lukasz A. Bartnik <l.bartnik@gmail.com>
2016-11-27 20:09:01 -0800

    remove old code

--- R/utils.r
+++
@@ -1,8 +0,0 @@
-#' @importFrom digest digest
-hash32 <- function (x) digest(x, 'xxhash32')
-
-#' @importFrom digest digest
-md5 <- function (x) digest(x, 'md5')
-
-
-is_lazy <- function (x) inherits(x, 'lazy')
